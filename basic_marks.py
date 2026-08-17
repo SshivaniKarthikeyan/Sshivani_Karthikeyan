510284e8a0b7ef3ecd1fe0cbb7a5780d4230216a
SshivaniKarthikeyan <sshivanikarthikeyan_bec28@mepcoeng.ac.in>
2026-08-17 10:37:21 +0530

    Create basic_marks.py to calculate aggregate and percentage score

---
+++ basic_marks.py
@@ -0,0 +1,10 @@
+#sum of marks and percentage of it
+S1=float(input("Enter mark of 1st subject:"))
+S2=float(input("Enter marks of 2nd subject:"))
+S3=float(input("Enter mark of 3rd subject:"))
+S4=float(input("Enter mark of 4th subject:"))
+S5=float(input("Enter mark of 5th subject:"))
+s=S1+S2+S3+S4+S5
+percentage=(s/500)*100
+print("Aggregate of marks is:",s)
+print("Percentage of marks is:",percentage)
